bc21176c250f2f02aff97f45358d280775afe067
UniqueXT <penghaitao918@163.com>
2022-08-19 14:37:41 +0800

    #添加注释

--- Android/BSDiff/bsdiff/src/main/java/com/uniquext/bsdiff/BSDiff.java
+++ Android/BSDiff/bsdiff/src/main/java/com/uniquext/bsdiff/BSDiff.java
@@ -2,6 +2,7 @@ package com.uniquext.bsdiff;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.net.Uri;
 import android.os.Build;
 
@@ -35,10 +36,27 @@ public class BSDiff {
         System.loadLibrary("bsdiff");
     }
 
+    /**
+     * 根据新旧 APK 文件生成对应补丁包
+     * @param oldApkPath 旧版本apk路径 {@link ApplicationInfo#sourceDir}
+     * @param newApkPath 新版本apk路径
+     * @param patchPath 目标补丁包
+     */
     public static native boolean bsdiff(String oldApkPath, String newApkPath, String patchPath);
 
+    /**
+     * 根据补丁包合成新版本 APK 文件
+     * @param oldApkPath 旧版本apk路径 {@link ApplicationInfo#sourceDir}
+     * @param newApkPath 目标新版本apk路径
+     * @param patchPath 补丁包
+     */
     public static native boolean bspatch(String oldApkPath, String newApkPath, String patchPath);
 
+    /**
+     * 安装 APK
+     * @param context 上下文
+     * @param apkFile apk文件
+     */
     public static void installApk(Context context, File apkFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
